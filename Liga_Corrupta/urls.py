4f2a5e7624387417770ec5743268e1ff748dd81b
Fundeanos03 <sergiomonpaj@gmail.com>
2026-04-17 17:55:07 +0200

    subida 4

--- Liga_Corrupta/urls.py
+++ Liga_Corrupta/urls.py
@@ -19,6 +19,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+
     path("", include("Liga_Corrupta_app.urls")),
+
     path("admin/", admin.site.urls),
 ]
--- Liga_Corrupta_app/views.py
+++ Liga_Corrupta_app/views.py
@@ -7,7 +7,9 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "inicio.html")
+
 def clasi(request):
     return render(request, "clasi.html")
+
 def mi_club(request):
     return render(request, "mi_club.html")
